c110bc98229f5eaa0c0373e03033d4868f8a17a9
Nico <jpegunderscore@gmail.com>
2019-07-25 12:00:15 +0200

    final

---
+++ app.rb
@@ -0,0 +1,8 @@
+require "color_text"
+require_relative "lib/board"
+require_relative "lib/board_case"
+require_relative "lib/game"
+require_relative "lib/player"
+
+game = Game.new # Start new game!
+game.go #Starting the game.
